09ce56a6e93708499a0772458ce0cd9e30647f6e
Jaguar-188 <ruturajlokhande9@gmail.com>
2023-06-18 13:13:04 +0530

    changed class structure and added extra members

--- src/main/kotlin/com/dank/dankProject/model/UserEducationHistory.kt
+++ src/main/kotlin/com/dank/dankProject/model/UserEducationHistory.kt
@@ -1,5 +1,6 @@
 package com.dank.dankProject.model
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
@@ -14,12 +15,22 @@ class UserEducationHistory() {
     lateinit var userId: String
     lateinit var publicId: String
     lateinit var institutionId: String
+    lateinit var institutionName: String
     lateinit var degree: String
+    lateinit var name: String
+    lateinit var email: String
+    lateinit var mobileNumber: String
 
-    constructor(userId: String, publicId: String,institutionId: String,degree: String) : this() {
+    @Autowired
+    constructor(userId: String, publicId: String,institutionId: String,institutionName: String,degree: String,name: String,
+                email: String,mobileNumber: String) : this() {
         this.userId = userId
         this.publicId = publicId
         this.institutionId = institutionId
+        this.institutionName = institutionName
         this.degree = degree
+        this.name = name
+        this.email = email
+        this.mobileNumber = mobileNumber
     }
 }
