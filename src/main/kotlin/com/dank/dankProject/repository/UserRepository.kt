be5ca8110eed02865d7f432f63e849098a1d928e
Jaguar-188 <ruturajlokhande9@gmail.com>
2023-06-18 13:14:41 +0530

    added implemention details of methods to fetch using instituteId

--- src/main/kotlin/com/dank/dankProject/repository/UserRepository.kt
+++ src/main/kotlin/com/dank/dankProject/repository/UserRepository.kt
@@ -2,8 +2,14 @@ package com.dank.dankProject.repository
 
 import com.dank.dankProject.model.UserEducationHistory
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : MongoRepository<UserEducationHistory,String> {
 
+    fun findByInstitutionId(@Param("institutionId") institutionId : String) : List<UserEducationHistory>
+
+    fun existsByInstitutionId(@Param("institutionId") institutionId : String) : Boolean
 
 }
